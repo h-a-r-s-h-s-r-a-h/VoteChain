98d388c0031b0930c1c8cde7108d14a00f911f41
Harsh <harshvirat894@gmail.com>
2025-03-27 05:09:50 +0530

    vote

--- anchor-voting-program/programs/anchor-voting-program/src/instructions/add_election.rs
+++ anchor-voting-program/programs/anchor-voting-program/src/instructions/add_election.rs
@@ -8,25 +8,25 @@ pub fn create_election(
     election_description: String,
 ) -> Result<()> {
     require!(
-        election_id.len() < MAX_ELECTIONID_LENGTH,
+        election_id.len() <= MAX_ELECTIONID_LENGTH,
         ElectionAccountError::ElectionIdTooLong
     );
 
     require!(
-        election_title.len() < MAX_TITLE_LENGTH,
+        election_title.len() <= MAX_TITLE_LENGTH,
         ElectionAccountError::ElectionTitleTooLong
     );
 
     require!(
-        election_description.len() < MAX_DESCRIPTION_LENGTH,
+        election_description.len() <= MAX_DESCRIPTION_LENGTH,
         ElectionAccountError::ElectionDescriptionTooLong
     );
 
     let election = &mut ctx.accounts.election;
     election.election_generator = ctx.accounts.election_generator.key();
-    election.election_id = election_id;
-    election.election_title = election_title;
-    election.election_description = election_description;
+    election.election_id = election_id.trim().to_string();
+    election.election_title = election_title.trim().to_string();
+    election.election_description = election_description.trim().to_string();
     election.is_active = true;
 
     msg!("Election added successfully!");
