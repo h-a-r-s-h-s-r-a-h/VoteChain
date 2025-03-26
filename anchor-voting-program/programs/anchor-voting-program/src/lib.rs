a4463b692a30044bcbd8468fef7843990b1a988e
Harsh <harshvirat894@gmail.com>
2025-03-27 01:26:53 +0530

    vote

---
+++ anchor-voting-program/programs/anchor-voting-program/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7QFYjVpv8vsSyxuiuLcTPpErcNkVZ1Liag8F9K5tqhVD");
+
+#[program]
+pub mod anchor_voting_program {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
