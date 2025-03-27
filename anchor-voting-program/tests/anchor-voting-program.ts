593c274693333c26b20177b94a06cd9b8de6e889
Harsh <harshvirat894@gmail.com>
2025-03-28 00:59:25 +0530

    vote

--- anchor-voting-program/tests/anchor-voting-program.ts
+++ anchor-voting-program/tests/anchor-voting-program.ts
@@ -15,13 +15,23 @@ describe("anchor-voting-program", () => {
     description: "PM election",
     election_id: "12345",
   };
+  const anotherElection = {
+    title: "Vidhan Sabha",
+    description: "CM Bihar election",
+    election_id: "123456",
+  };
+
+  const [anotherElectionPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("election"), Buffer.from(anotherElection.election_id)],
+    program.programId
+  );
 
   const [electionPda] = anchor.web3.PublicKey.findProgramAddressSync(
     [Buffer.from("election"), Buffer.from(election.election_id)],
     program.programId
   );
 
-  it("adds a movie review", async () => {
+  it("adds a election", async () => {
     try {
       await program.methods
         .createElection(
@@ -38,6 +48,32 @@ describe("anchor-voting-program", () => {
       expect(account.electionId).to.equal(election.election_id);
       expect(account.electionTitle).to.equal(election.title);
       expect(account.electionDescription).to.equal(election.description);
+      expect(account.electionGenerator.toString()).to.equal(
+        provider.wallet.publicKey.toString()
+      );
+    } catch (error) {
+      console.error("Error adding movie review:", error);
+      throw error;
+    }
+  });
+
+  it("adds another election", async () => {
+    try {
+      await program.methods
+        .createElection(
+          anotherElection.election_id,
+          anotherElection.title,
+          anotherElection.description
+        )
+        .accounts({})
+        .rpc();
+
+      const account = await program.account.electionAccountState.fetch(
+        anotherElectionPda
+      );
+      expect(account.electionId).to.equal(anotherElection.election_id);
+      expect(account.electionTitle).to.equal(anotherElection.title);
+      expect(account.electionDescription).to.equal(anotherElection.description);
       expect(account.electionGenerator.toString()).to.equal(
         provider.wallet.publicKey.toString()
       );
