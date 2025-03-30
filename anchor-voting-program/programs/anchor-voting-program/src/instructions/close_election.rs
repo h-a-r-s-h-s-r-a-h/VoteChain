78a1444f01c0c57c26bbd3c8c575e8a19c2cc220
Harsh <harshvirat894@gmail.com>
2025-03-31 01:11:42 +0530

    voting

--- anchor-voting-program/programs/anchor-voting-program/src/instructions/close_election.rs
+++ anchor-voting-program/programs/anchor-voting-program/src/instructions/close_election.rs
@@ -1,2 +1,14 @@
-use crate::{constants::*, contexts::*, errors::*};
+use crate::{constants::*, contexts::*, errors::*, state::election_account};
 use anchor_lang::prelude::*;
+
+pub fn close_election(ctx: Context<CloseElection>, election_id: String) -> Result<()> {
+    let election = &mut ctx.accounts.election;
+    require!(
+        election.is_active == true,
+        CloseElectionError::ElectionClosedEarlier
+    );
+
+    election.is_active = false;
+    msg!("Election closed successfully!");
+    Ok(())
+}
